de2e472ccc03e874cd80fb8c7dcd1f8cf3b2d3e4
jagadish7-25 <kadapajag7@gmail.com>
2024-07-09 12:12:51 +0200

    dns name

--- server.tf
+++ server.tf
@@ -1,9 +1,19 @@
 resource "aws_instance" "terraform-R" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [ "sg-0d2a6d1a39ee8b441" ]
+  vpc_security_group_ids = [ "sg-09c956a01686bdc1d" ]
 
   tags = {
     Name = "terraform-R"
   }
+}
+
+resource "aws_route53_record" "frontend" {
+  name            = "frontend.me25.online"
+  ttl             = 30
+  type            = "A"
+  zone_id         = "Z0674306Y5K4DOUDCVST"
+
+  records = [aws_instance.terraform-R.private_ip]
+  
 }
