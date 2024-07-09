4f5e129f011da3ee31155717507651cd2bbc609d
jagadish7-25 <kadapajag7@gmail.com>
2024-07-09 12:58:45 +0200

    changed

--- vars/samplev.tf
+++ vars/samplev.tf
@@ -3,7 +3,38 @@ variable "instance_type" {
   
 }
 
-output "instance_type" {
+variable "instance_types" {
+    default = [ "t2.micro", "t3.medium", "c5.large"]
+  
+}
+
+variable "instance_type_cost" {
+    default = {
+        t2.micro = 100
+        t3.medium = 200
+        c5.large = 400
+    }
+  
+}
+
+variable "instance_type_cost_mem" {
+    default = {
+        t2.micro = {
+            cost = 100
+            mem = 20
+        }
+        t3.medium = {
+            cost = 200
+            mem = 40
+        }
+        c5.large = {
+            cost = 400
+            mem = 150
+        }
+    }
+  
+}
+output "instance_type_cost_mem" {
     value= var.instance_type
   
 }
