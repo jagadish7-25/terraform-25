90ad3e91a83ae14546841bbc06d8f02b6b111b15
jagadish7-25 <kadapajag7@gmail.com>
2024-07-09 11:43:03 +0200

    added sg

--- server.tf
+++ server.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sgr-055671cdd78bcae0b" ]
 
   tags = {
     Name = "terraform-R"
