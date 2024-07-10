e747daf3a9f14549c52c80b9430af22b35fa2a28
jagadish7-25 <kadapajag7@gmail.com>
2024-07-10 11:05:39 +0200

    latest vars

---
+++ var/main.tf
@@ -0,0 +1,18 @@
+resource "aws_instance" "var" {
+  ami           = "ami-0b4f379183e5706b9"
+  instance_type = "var.instance"
+
+  tags = {
+    Name = "var.name"
+  }
+}
+
+  variable "instance" {
+    default = t2.micro
+    
+  }
+
+  variable "name" {
+    default = jack-1
+    
+  }
