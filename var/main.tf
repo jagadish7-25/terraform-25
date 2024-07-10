a778ecc5f9b336610c15c541d1265a3c656c3416
jagadish7-25 <kadapajag7@gmail.com>
2024-07-10 12:24:36 +0200

    sv

--- var/main.tf
+++ var/main.tf
@@ -2,8 +2,10 @@ resource "aws_instance" "latest" {
   ami           = "ami-0b4f379183e5706b9"
   instance_type = var.instance
 
-  tags = {
-    Name = var.name
+  
+
+   tags = {
+    Name = var.env_name
   }
 }
 
--- var/stage.tfvars
+++ var/stage.tfvars
@@ -1,3 +1,3 @@
 env_name = "stage"
-name = "jack-25"
+
 instance = "t2.micro"
