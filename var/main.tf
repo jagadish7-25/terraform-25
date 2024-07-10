4816178fc73fdf5a264ad36d790dbb51c418762f
jagadish7-25 <kadapajag7@gmail.com>
2024-07-10 11:11:05 +0200

    modi

--- var/main.tf
+++ var/main.tf
@@ -1,18 +1,20 @@
 resource "aws_instance" "latest" {
   ami           = "ami-0b4f379183e5706b9"
-  instance_type = "var.instance"
+  instance_type = var.instance
 
   tags = {
-    Name = "var.name"
+    Name = var.name
   }
 }
 
+
+
   variable "instance" {
-    default = t2.micro
+    default = "t2.micro"
     
   }
 
   variable "name" {
-    default = jack-1
+    default = "jack-1"
     
   }
